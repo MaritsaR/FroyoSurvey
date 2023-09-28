7c50b79b128ed0a7fdaf038218df6028523c6687
Maritsa <maritsar28@gmail.com>
2023-09-27 19:44:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+// console.log(someVehicle["model"])
+// Froyo store management wants a survey to gather info from their customers
+// first name, last name, email, phone, zip code, favorite flavors, cup size, favorite store, first visit
+// create an object with those keys using supplied test info
+
+const customer = {
+    firstName: "jake",
+    lastName: "smith",
+    email: "jaekSmih!aol.com",
+    phone: undefined,
+    zipCode: "631",
+    favoriteFlavors: 32,
+    cupSize: "medium",
+    favoriteStore: "Target",
+    firstVisit: false,
+   
+};
+console.log(customer);
+
+customer['email'] = "Jak3Smith1992@email.com";
+
+
+customer['phone'] = 3195551234;
+
+
+customer['zipCode'] = "63132";
+console.log("63132");
+
+customer['favoriteFlavors'] = ["coffe", "strawberry", "matcha"];
+
+console.log(customer);
+
+delete customer ['zipCode'];
+delete customer ['favoriteStore'];
+
+console.log(customer);
+
+customer.toppings = ["chocolate sprinkles", "wafer straws", "gummy bear"];
+customer.futureFlavors = 'mango';
+customer.todaysPurchaseCost = 5.32;
+
+console.log(customer);
+
+const surveyKeys = Object.keys(customer);
+
+console.log(surveyKeys);
---
+++ practice.js
@@ -0,0 +1,9 @@
+// const someVehicle= {
+//     make: "Ford",
+//     model: 'F150',
+//     type: 'Pickup Truck',
+// };
+
+// console.log(someVehicle.make);
+
+// //We get the value of the make because that's what we console.log it
